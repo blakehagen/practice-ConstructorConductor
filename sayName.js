ed7d7739d0f3a8beff74278a5f031b5955eb1afb
Blake Hagen <blakehagen@gmail.com>
2015-01-21 19:42:42 -0700

    wed 745p

--- sayName.js
+++ sayName.js
@@ -2,12 +2,28 @@
 
   //code here
 
+var Person = function(name, age) {
+	this.name = name;
+	this.age = age;
+}
 
 //Now create three instances of Person with data you make up
 
   //code here
 
+ var blake = new Person ("Blake", 32);
+ var emily = new Person ("Emily", 30);
+ var taylor = new Person ("Taylor", 6);
+
 
 //Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
 
   //code here
+
+Person.prototype.sayName = function() {
+	alert(this.name);
+};
+
+blake.sayName();
+
+
